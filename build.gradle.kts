90072e848722cbe60f99ba74fee5ff2140c5929c
teyyubismayil <teyyub.ismayilov@mail.ru>
2021-10-20 17:00:38 +0400

    to jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.springframework.boot.gradle.tasks.bundling.BootWar
 
 plugins {
     id("org.springframework.boot") version "2.4.5"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    war
     kotlin("jvm") version "1.4.32"
     kotlin("kapt") version "1.4.32"
     kotlin("plugin.spring") version "1.4.32"
@@ -14,8 +12,8 @@ group = "com.teyyub"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
-tasks.getByName<BootWar>("bootWar") {
-    archiveFileName.set("TestService.war")
+tasks.bootJar {
+    archiveFileName.set("app.jar")
 }
 
 repositories {
@@ -32,7 +30,6 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     runtimeOnly("com.h2database:h2")
-    providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.junit.jupiter:junit-jupiter:5.4.2")
 }
