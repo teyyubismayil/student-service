9d1e1d725cd76347d631d30a7344745dca038d42
teyyubismayil <teyyub.ismayilov@mail.ru>
2021-10-23 13:43:14 +0400

    profiles

--- src/main/kotlin/com/teyyub/testservice/student/repository/StudentRepository.kt
+++ src/main/kotlin/com/teyyub/testservice/student/repository/StudentRepository.kt
@@ -4,5 +4,4 @@ import com.teyyub.testservice.student.model.entity.Student
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
-@Repository
 interface StudentRepository: JpaRepository<Student, Int>
